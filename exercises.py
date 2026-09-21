0f6d4e01cce2d49307356fe1431398705004ceb5
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-09-21 21:01:04 +0300

    Ex6

--- exercises.py
+++ exercises.py
@@ -48,3 +48,16 @@ def sum_to(n):
 
 print('Exercise 5:', sum_to(6))
 
+# Exercise 6: Find the Largest Number
+
+def largest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print('Exercise 6:', largest(1, 2, 3))
+
